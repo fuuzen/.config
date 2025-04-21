2ab977638a8490760abcece6a6f2259438bf844a
fuuzen <80118151@qq.com>
2025-04-21 06:33:43 +0000

    fix: neofetch sh script

--- neofetch/source_me.sh
+++ neofetch/source_me.sh
@@ -6,18 +6,21 @@
 source "${HOME}/.config/neofetch/image.sh"
 
 if [[ "${IMG_NAME}" == *"random"* ]]; then
-  NEOFETCH_IMG_PATH=/tmp/${IMG_NAME}
-  export NEOFETCH_CHAFA_PATH=/tmp/${IMG_NAME}.chafa
+  neofetch --clean
+  NEOFETCH_IMG_PATH="/tmp/${IMG_NAME}"
+  export NEOFETCH_CHAFA_PATH="/tmp/${IMG_NAME}.chafa"
 else
-  NEOFETCH_IMG_PATH=${HOME}/.config/neofetch/${IMG_NAME}
-  export NEOFETCH_CHAFA_PATH=${HOME}/.config/neofetch/${IMG_NAME}.chafa
+  NEOFETCH_IMG_PATH=${HOME}/.config/images_chafa/${IMG_NAME}
+  export NEOFETCH_CHAFA_PATH=${HOME}/.config/images_chafa/${IMG_NAME}.chafa
 fi
 
-if [ ! -f "${NEOFETCH_CHAFA_PATH}" ]; then
-  if [ ! -f "${NEOFETCH_IMG_PATH}" ]; then
+if [ ! -f "${NEOFETCH_CHAFA_PATH}" ] || [[ "${IMG_NAME}" == *"random"* ]]; then
+  if [ ! -f "${NEOFETCH_IMG_PATH}" ] || [[ "${IMG_NAME}" == *"random"* ]]; then
     curl -so "${NEOFETCH_IMG_PATH}" "${IMG_URL}"
   fi
   chafa "${NEOFETCH_IMG_PATH}" -f symbols -s 50 > "${NEOFETCH_CHAFA_PATH}"
 fi
+
+alias neofetch='neofetch --chafa --source ${NEOFETCH_IMG_PATH}'
 
 neofetch
